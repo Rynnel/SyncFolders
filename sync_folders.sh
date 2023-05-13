76fe7c7d6b2e582fccf531c2e2c6dea845279db1
Rynnel <81760746+Rynnel@users.noreply.github.com>
2023-05-13 11:12:21 +0300

    Update sync_folders.sh

--- sync_folders.sh
+++ sync_folders.sh
@@ -4,14 +4,14 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
-if [[ ! -d "$1" ]]; then
+if [[ ! -d $1 ]]; then
   echo "Main folder doesn't exist.."
   echo "I can't synchronize files from non-existing folder."
   exit 1
 fi
 
-for folder in "${@}"; do
-  if [ ! -d "$folder" ]; then
+for folder in ${@}; do
+  if [ ! -d $folder ]; then
     echo "Folder $folder doesn't exist. Creating.."
     echo ""
     mkdir $folder
@@ -19,8 +19,8 @@ for folder in "${@}"; do
   fi
 done
 
-for i in "${@}"; do
-  for j in "${@}"; do
+for i in $@; do
+  for j in $@; do
     if [[ $i != $j ]]; then
       rsync -avh --delete "$i/" "$j/"
     fi
