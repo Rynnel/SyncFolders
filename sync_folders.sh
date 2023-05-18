09933bf7717bbd4550dea89de631dd76b9c62caf
Rynnel <81760746+Rynnel@users.noreply.github.com>
2023-05-18 10:26:47 +0300

    UPD

--- sync_folders.sh
+++ sync_folders.sh
@@ -1,4 +1,3 @@
-
 if [ $# -lt 2 ]; then
   echo "Usage: $0 main_folder(files will be synchronized from here) folder2...folderN"
   exit 1
@@ -10,19 +9,16 @@ if [[ ! -d $1 ]]; then
   exit 1
 fi
 
-for folder in ${@}; do
+for folder in $@; do
   if [ ! -d $folder ]; then
     echo "Folder $folder doesn't exist. Creating.."
     echo ""
     mkdir $folder
-
   fi
 done
 
-for i in $@; do
-  for j in $@; do
-    if [[ $i != $j ]]; then
-      rsync -avh --delete "$i/" "$j/"
-    fi
-  done
+source=$1
+
+for sync_folder in "${@:2}"; do
+  rsync -avh --delete "$source/" "$sync_folder/"
 done
