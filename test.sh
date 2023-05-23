5def1ad401527ac1e699ab503f9a57ce3d3dc010
Rynnel <restle220@mail.ru>
2023-05-23 11:36:32 +0300

    commit

--- test.sh
+++ test.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 chmod +x sync_folders.sh
 
-# Тест для проверки работы скрипта, если не указаны папки. Скрипт должен вывести "Usage: ..."
+
+# Тест для проверки работы скрипта, если не указаны папки.
 echo "---ТЕСТ 1. Если не указаны аргументы---"
 echo ""
-echo "./sync_folders.sh"
 ./sync_folders.sh
+if [[ ! -n $1 ]]; then
+    echo "Тест пройден!"
+else
+    echo "Тест провален."
+    exit 1
+fi
 echo ""
 
 # Тест для проверки работы скрипта, если указана несуществующая главная папка
 echo "---ТЕСТ 2. Если главная папка не существует---"
 echo ""
-echo "./sync_folders.sh non_existent/ folder1"
-./sync_folders.sh non_existent/ folder1
+./sync_folders.sh non_existent folder1
+if [[ ! -d $1 ]]; then
+    echo "Тест пройден!"
+else
+    echo "Тест провален."
+    exit 1
+fi
 echo ""
 
-
 # Тест для проверки работы скрипта, если указана несуществующая папка. Скрипт должен создать эту папку и провести синхронизацию
-echo "---ТЕСТ 3. Если указана несуществующая папка для синхронизации---"
+echo "---ТЕСТ 3. Если указано несколько папок для синхронизации---"
 echo ""
 echo "./sync_folders.sh testdir folder2"
 ./sync_folders.sh testdir folder2
@@ -27,36 +37,17 @@ touch file1.txt file2.txt
 cd ..
 echo "./sync_folders.sh testdir folder2"
 ./sync_folders.sh testdir folder2
-echo "Файлы в папке folder2:"
-cd folder2
-ls
-cd ..
+
+
+code=$(diff -q testdir folder2)
+if [[ $code -eq 0 ]]; then
+    echo "Тест пройден!"
+else
+    echo "Тест провален."
+    exit 1
+fi
 echo ""
 
-# Тест для проверки работы скрипта, если указано больше 2 папок
-echo "---ТЕСТ 4. Если указано больше 2 папок---"
-echo ""
-
-echo "До создания файла file3.txt: "
-echo "./sync_folders.sh testdir folder2 folder3 folder4"
-./sync_folders.sh testdir folder2 folder3 folder4
-echo "Файлы в папке testdir:"
+rm -r folder2
 cd testdir
-ls
-echo ""
-echo "Создание файла file3.txt.."
-touch file3.txt
-cd ..
-echo ""
-
-echo "После создания файла file3.txt: "
-echo "./sync_folders.sh testdir folder2 folder3 folder4"
-./sync_folders.sh testdir folder2 folder3 folder4
-cd folder4
-echo "Файлы в folder4:"
-ls
-
-cd ..
-rm -r folder2 folder3 folder4
-cd testdir
-rm file1.txt file2.txt file3.txt
+rm file1.txt file2.txt
